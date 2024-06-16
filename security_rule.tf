d1fd37092dd09f2862cfa141300d077153f1c432
rilwan-o <opeayoriliwan@yahoo.com>
2024-06-16 19:45:48 +0100

    enable port 3000

--- security_rule.tf
+++ security_rule.tf
@@ -1,8 +1,8 @@
 # Allow traffic from the load balancer to the instances
 resource "aws_security_group_rule" "instance_from_lb" {
   type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
+  from_port                = 3000
+  to_port                  = 3000
   protocol                 = "tcp"
   security_group_id        = aws_security_group.instance_sg.id
   source_security_group_id = aws_security_group.sg.id
