f1b5b01619b8fb80d6c61b1a6c27b924f8ea3093
rilwan-o <opeayoriliwan@yahoo.com>
2024-06-16 18:32:42 +0100

    add policy

--- s3bucket.tf
+++ s3bucket.tf
@@ -17,6 +17,15 @@ resource "aws_dynamodb_table" "terraform_locks" {
 
 
 
+resource "aws_s3_bucket" "web_config_bucket" {
+  bucket = "groupc-web-config-bucket"
+}
+
+resource "aws_sns_topic" "web_notifications" {
+  name = "web-notifications"
+}
+
+
 
 resource "aws_iam_role" "web_role" {
   name = "web-role"
@@ -33,26 +42,54 @@ resource "aws_iam_role" "web_role" {
   })
 }
 
-
-resource "aws_s3_bucket" "web_config_bucket" {
-  bucket = "groupc-web-config-bucket"
-}
-
-resource "aws_sns_topic" "web_notifications" {
-  name = "web-notifications"
-}
-
-resource "aws_iam_role" "web_iam_role" {
-  name = "web-iam-role"
-
-  assume_role_policy = jsonencode({
+resource "aws_iam_policy" "s3_policy" {
+  name        = "S3AccessPolicy"
+  description = "Policy for S3 PUT/GET access"
+  policy      = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
+      Action = [
+        "s3:PutObject",
+        "s3:GetObject"
+      ]
+      Effect   = "Allow"
+      Resource = "arn:aws:s3:::your-bucket-name/*"
     }]
   })
+}
+
+resource "aws_iam_policy" "ssm_policy" {
+  name        = "SSMAccessPolicy"
+  description = "Policy for Session Manager access"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = [
+        "ssm:StartSession",
+        "ssm:SendCommand",
+        "ssm:DescribeInstanceInformation",
+        "ssm:DescribeSessions",
+        "ssm:GetDeployablePatchSnapshotForInstance",
+        "ssm:GetDocument",
+        "ssm:ListInstanceAssociations",
+        "ssm:ListAssociations",
+        "ssm:ListDocuments",
+        "ssm:ListTagsForResource",
+        "ssm:PutInventory",
+        "ssm:UpdateInstanceInformation"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
+  role       = aws_iam_role.web_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ssm_policy" {
+  role       = aws_iam_role.web_role.name
+  policy_arn = aws_iam_policy.ssm_policy.arn
 }
