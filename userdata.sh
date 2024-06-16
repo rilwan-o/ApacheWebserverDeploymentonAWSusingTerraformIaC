f6207f345c64b054a4fe0111078874f0843569f8
rilwan-o <opeayoriliwan@yahoo.com>
2024-06-16 20:17:14 +0100

    start app

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y httpd
-# Install Node.js and npm from NodeSource
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
-sudo yum install -y nodejs
 sudo service httpd start
 sudo chkconfig httpd on
 git clone https://github.com/cloudboostauk/webApp-RQGen-A.git
-cd webApp-Quizz-B
-npm install
-npm start
+sudo cp -R webApp-Quizz-B/ /var/www/html/
+sudo chmod -R 755 /var/www/html/webApp-Quizz-B
+sudo chown -R apache:apache /var/www/html/webApp-Quizz-B
+sudo service httpd start
+
