ff5cbb56fe54a915f8b0b0700809af3125f3662c
rilwan-o <opeayoriliwan@yahoo.com>
2024-06-16 20:47:54 +0100

    install git

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y httpd
+sudo yum install git
 sudo service httpd start
 sudo chkconfig httpd on
 git clone https://github.com/cloudboostauk/webApp-RQGen-A.git
-sudo cp -R webApp-Quizz-B/ /var/www/html/
+sudo cp -R /webApp-Quizz-B/ /var/www/html/
 sudo chmod -R 755 /var/www/html/webApp-Quizz-B
 sudo chown -R apache:apache /var/www/html/webApp-Quizz-B
 sudo service httpd start
