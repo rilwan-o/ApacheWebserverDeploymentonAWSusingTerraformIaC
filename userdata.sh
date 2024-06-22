d0b4e44dba36bd3209fb3f7273292ff4f0f35b9c
rilwan-o <opeayoriliwan@yahoo.com>
2024-06-22 17:29:35 +0100

    install git

--- userdata.sh
+++ userdata.sh
@@ -4,7 +4,7 @@ sudo yum install -y httpd
 sudo yum install git
 sudo service httpd start
 sudo chkconfig httpd on
-sudo yum install git -y
+sudo yum install git -y #install git
 git clone https://github.com/cloudboostauk/webApp-RQGen-A.git
 sudo cp -R /webApp-RQGen-A/ /var/www/html/
 sudo chmod -R 755 /var/www/html/webApp-RQGen-A #move app folder to html
