882ec89186b8282059bb63181adcd59decbba28c
rilwan-o <opeayoriliwan@yahoo.com>
2024-06-22 16:43:46 +0100

    add app folder and file

--- userdata.sh
+++ userdata.sh
@@ -6,7 +6,7 @@ sudo service httpd start
 sudo chkconfig httpd on
 git clone https://github.com/cloudboostauk/webApp-RQGen-A.git
 sudo cp -R /webApp-RQGen-A/ /var/www/html/
-sudo chmod -R 755 /var/www/html/webApp-RQGen-A
+sudo chmod -R 755 /var/www/html/webApp-RQGen-A #move app folder to html
 sudo chown -R apache:apache /var/www/html/webApp-RQGen-A
 sudo service httpd start
 
